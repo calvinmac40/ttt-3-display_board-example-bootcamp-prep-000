de95c50ba2dea631ae7062bd0df186c08e7403fd
Calvin McDowell <calvinmac40@hotmail.com>
2020-03-23 12:58:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board =  "  |   |   \n"
-  "  |   |   \n"
-  "  |  |  "
+  board = 3.times{puts "  |  |  "}
   puts(board)
 end
